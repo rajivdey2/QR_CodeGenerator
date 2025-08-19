7b6ed1369bae0b34d411b9c0121bb7bacaf179b6
rajivdey2 <deyrajiv2572@gmail.com>
2025-08-19 19:43:14 +0530

    done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,40 +1,45 @@
-import React , {useState} from 'react';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [text , setText]= useState("");
+  const [text, setText] = useState("");
   const [data, setData] = useState("");
-  const fetchData = async () => {
-    const res = await fetch("https://qr-codegenerator.onrender.com/api",{
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({text: text})
-    })
-    const data = await  res.json();
 
-    if (data){
-      console.log(data);
-      setData(data);
-      
-    }
+  const fetchData = async () => {
+    const res = await fetch("https://qr-codegenerator.onrender.com/api", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ text }),
+    });
+    const data = await res.json();
+    if (data) setData(data);
   };
 
   return (
-    <div>
-      <input type = "text" placeholder = "Enter URL" onChange={(e) =>{setText(e.target.value);}} value= {text}></input>
-      <button onClick={() => {fetchData();}}>Generate QR Code</button>
-      <button>
-        {data ? (
-          <div>
-              <img src={`data:image/png;base64,${data.data}`} alt="QR Code" />
-              <a href={`data:image/png;base64,${data.data}`} download="qrcode.png">Download QR Code image</a>
-
-          </div>
-        ):null}
-      </button>
+    <div className="container">
+      <h1>🎛 QR Retro Generator 🎶</h1>
+      <input
+        type="text"
+        placeholder="Enter URL..."
+        onChange={(e) => setText(e.target.value)}
+        value={text}
+      />
+      <div>
+        <button onClick={fetchData}>Generate</button>
+      </div>
+      {data ? (
+        <div className="qr-container">
+          <img src={`data:image/png;base64,${data.data}`} alt="QR Code" />
+          <a
+            href={`data:image/png;base64,${data.data}`}
+            download="qrcode.png"
+          >
+            ⬇ Download QR Code
+          </a>
+        </div>
+      ) : null}
     </div>
   );
 }
 
-export default App
+export default App;
